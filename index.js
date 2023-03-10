3493dddc6d043b440eab2aad25d5e05a40f06fb5
THINESHKUMAR <thinesh12@gmal.com>
2023-03-10 00:12:27 -0500

    edit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messageNames = [];
+    for (let i = 0; i < names.length; i++) {
+       messageNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messageNames;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
